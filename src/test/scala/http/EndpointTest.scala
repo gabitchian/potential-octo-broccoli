6e35b4d3a2d839d7911a380c6eddbf8decbe34e8
Giacomo Magri <giacomo.magri@dextra-sw.com>
2021-03-09 20:14:48 -0300

    refatorei o EndpointTest

--- src/test/scala/http/EndpointTest.scala
+++ src/test/scala/http/EndpointTest.scala
@@ -1,27 +1,19 @@
 package http
 
-import akka.http.scaladsl.model.StatusCodes
-import akka.http.scaladsl.model.headers.{BasicHttpCredentials, OAuth2BearerToken}
 import akka.http.scaladsl.server.Route
-import akka.http.scaladsl.testkit.{RouteTestTimeout, ScalatestRouteTest}
-import akka.stream.Materializer
-import io.circe.Decoder.Result
-import org.scalatest.{BeforeAndAfter, FeatureSpec, Matchers}
+import akka.http.scaladsl.testkit.ScalatestRouteTest
+import org.scalatest.BeforeAndAfter
+import org.scalatest.featurespec.AnyFeatureSpec
 
-import io.circe.Json
-import io.circe.optics.JsonPath
-import io.circe.optics.JsonPath._
-
-import scala.concurrent.duration._
 import scala.language.postfixOps
 
-class EndpointTest extends FeatureSpec with BeforeAndAfter with ScalatestRouteTest with Matchers {
+class EndpointTest extends AnyFeatureSpec with BeforeAndAfter with ScalatestRouteTest {
   lazy val endpoints: Endpoint = new Endpoint()
 
   protected def routes: Route = endpoints.routes
 
-  feature("Hello world endpoint") {
-    scenario("get") {
+  Feature("Hello world endpoint") {
+    Scenario("get") {
       Get("/") ~> routes ~> check {}
     }
   }
